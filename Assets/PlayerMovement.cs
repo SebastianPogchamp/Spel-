c2289bcf1cbd2426535de5a1a9415bd94754fde5
TheoAmnert <98454379+TheoAmnert@users.noreply.github.com>
2022-05-18 09:24:49 +0200

    Bögporr

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    private float Move;
+    public float jump;
+    public bool IsJumping;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsJumping == false) ;
+
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            Debug.Log("jump");
+        }
+
     }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            IsJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        IsJumping = true;
+    }
+
+
 }
+
